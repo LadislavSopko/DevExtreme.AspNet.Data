5739979c0235088e1b894a20614029c3f59d95e9
Ladislav Sopko <lsopko@3di.it>
2024-09-05 14:10:52 +0200

    fixed after merge

--- net/DevExtreme.AspNet.Data/AssemblyInfo.cs
+++ net/DevExtreme.AspNet.Data/AssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-#if DEBUG
+//#if DEBUG
 [assembly: InternalsVisibleTo("DevExtreme.AspNet.Data.Tests")]
 [assembly: InternalsVisibleTo("DevExtreme.AspNet.Data.Tests.Common")]
 // [assembly: InternalsVisibleTo("DevExtreme.AspNet.Data.Tests.Xpo")]
-#endif
+//#endif
 [assembly: CLSCompliant(true)]
 
 //#if !DEBUG
